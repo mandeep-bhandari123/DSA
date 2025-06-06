5ca501aba6748484428caa34c326507d69a62378
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-06-06 19:10:42 +0545

    Code update

--- dynamic_programming/coin_change.py
+++ dynamic_programming/coin_change.py
@@ -1,10 +1,10 @@
 def number_of_coin(coins,amount):
     dp=[float("inf") for i in range(amount+1)]#creating a list upto amount and keeping each number of coin infinity cuz we don't know real number of coin required
     dp[0]=0#base case if the amount is zero we need zero coins
-    for i in range (amount+1):#To got the coins required from amount  0 to amount given by user
+    for i in range (amount+1):#To get the number of coins required from amount  0 to amount given by user
         for coin in coins:# To check which coin is more suitable to use
             if coin<= amount:# To check wether the coin is greater then amount or not
-                dp[i]=min(dp[i],dp[i-coin]+1)#Checks given the minimum number of coins required 
+                dp[i]=min(dp[i],dp[i-coin]+1)#Checks given the minimum number of coins required for ammount i
     
     return dp[amount]#Returinig the minimum number of coins 
 
